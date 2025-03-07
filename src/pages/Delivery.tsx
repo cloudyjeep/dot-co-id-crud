d284b8f51ca55ec6c70bcc17c8c0e4bd4fd0c787
Razif Affandi <cloudyjeep@gmail.com>
2025-03-07 10:15:26 +0700

    dot

--- src/pages/Delivery.tsx
+++ src/pages/Delivery.tsx
@@ -1,8 +1,13 @@
 import { ClockAlert } from "lucide-react";
 import { Section } from "../components/Custom";
 import { Typography } from "@mui/material";
+import { useDataProvince } from "../api/delivery";
 
 const Delivery = () => {
+  const { data } = useDataProvince();
+  //   data?.results?.map
+  console.log("Deliv: ", data?.results);
+  
   return (
     <Section title="Delivery">
       <Typography display="flex" gap={2} py={1} color="error" fontWeight={600}>
